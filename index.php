21af3a5680a3c3417c968030ce575297b54b702c
Melvin <melzkaizer@gmail.com>
2021-03-09 11:31:26 +0800

    added melvin to welcome page

--- index.php
+++ index.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Melvin's First Git</title>
+    <title>Melvin's Git</title>
 </head>
 <body>
     <h1> Parang Tanga lang</h1>
     <p> nahulog sa kanal</p>
+    <p> 123</p>
 </body>
 </html>
